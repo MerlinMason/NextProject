4c9cae86206df1ac28d82c5094482b6afc465bf1
Merlin Mason <merlin.mason@healios.org.uk>
2021-11-18 20:26:39 +0000

    Fix linting issues

--- components/__tests__/Features.test.js
+++ components/__tests__/Features.test.js
@@ -4,6 +4,6 @@ import Features from "../Features";
 describe("<Features />", () => {
     it("Should list Jest as a feature", () => {
         const { getByText } = render(<Features />);
-        expect(getByText("Jest (with Testing Library)"));
+        expect(getByText("Jest (with Testing Library)")).toBeInTheDocument();
     });
 });
