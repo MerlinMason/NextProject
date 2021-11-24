b0cf0616e6116e50c1d26410ab93f7ebefc03ff7
Merlin Mason <merlin.mason@healios.org.uk>
2021-11-24 11:52:02 +0000

    Support .api file name extensions

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ module.exports = {
     images: {
         domains: [],
     },
-    // only route files with .page naming convention, allows colocating of tests and other supporting files
-    pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+    // only route files with .page or .api naming convention, allows colocating of tests and other supporting files within /pages
+    pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js", "api.ts", "api.js"],
     // set the the html lang attr
     i18n: {
         locales: ["en"],
